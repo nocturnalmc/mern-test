fc655f11a081be8e0546c13da4740a45318d0e90
noct <ezy_95@outlook.com>
2023-03-27 22:11:09 +0800

    fix: fixing route for serving client

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static('./22-3-2023/dist'));
+const root = path.join(__dirname, '22-3-2023', 'dist');
+app.use(express.static(root));
 
 app.get('/generate', (req, res) => {
   res.status(200).json({ generate: 'Successfull' });
@@ -12,8 +14,12 @@ app.get('/api/v1', (req, res) => {
   res.status(200).json({ msg: 'This is API v1' });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '22-3-2023', 'dist', 'index.html'));
+});
+
 app.all('*', (req, res) => {
-  res.status(404).send('Not found');
+  res.status(404).json({ msg: 'Error: 404, route does not exist' });
 });
 
 const port = process.env.PORT || 7500;
